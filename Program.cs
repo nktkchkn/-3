31f6e837a8f8604a2b14a92e052f677b65ade5f3
Nikita Kochkin <nktkchkn21@yandex.ru>
2023-02-26 01:11:53 +0300

    Решил домашнюю задачу 19

--- Program.cs
+++ Program.cs
@@ -6,3 +6,26 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine()!);
+int a1 = num / 10000;
+int a2 = num / 1000 % 10;
+int a4 = num / 10 % 10;
+int a5 = num % 10;
+// Console.WriteLine($"{a1} {a2} {a3} {a4} {a5}");
+if (num <= 100000 && num >= 10000)
+{
+    if (a1 == a5 && a2 == a4)
+    {
+        Console.Write("Да");
+    }
+    else
+    {
+        Console.Write("Нет");
+    
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
